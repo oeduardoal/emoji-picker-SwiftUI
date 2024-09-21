3a408a5c1ba3461ca80e127a4a9d57d34f354157
Eduardo Almeida <oeduardoal@gmail.com>
2024-09-21 14:44:45 -0300

    Initial Commit

---
+++ EmojiPicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiPicker
+//
+//  Created by Eduardo Almeida on 21/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmojiPicker/EmojiPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPickerApp.swift
+//  EmojiPicker
+//
+//  Created by Eduardo Almeida on 21/09/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
